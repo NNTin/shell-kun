8e80f0e5692d04181641c3f78fbef1a0f75400c2
geraskalnas <geraskalnas@gmail.com>
2020-06-05 15:08:00 +0300

    Comments and vim fix

--- login.sh
+++ login.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+#env variables by apt
 cd .profile.d;
 source *;
 
+#preparing screen
 mkdir -p $HOME/.apt/var/run/screen;
 export SCREENDIR=$HOME/.apt/var/run/screen;
+
+#preparing vim
+link $HOME/.apt/usr/bin/vim.basic $HOME/.apt/usr/bin/vim
 
 bash;
